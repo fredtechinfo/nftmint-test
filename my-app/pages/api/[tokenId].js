40342d98e46bce6029c5fe8105598a60586414e6
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-07-08 20:52:33 +0545

    image

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -1,17 +1,10 @@
 export default function handler(req, res) {
-    // get the tokenId from the query params
     const tokenId = req.query.tokenId;
-    // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "https://raw.githubusercontent.com/haripandey21/Nfts-minting-dapp-rinkeby_testnet/main/my-app/public/cryptodevs/";
-    // The api is sending back metadata for a Crypto Dev
-    // To make our collection compatible with Opensea, we need to follow some Metadata standards
-    // when sending back the response from the api
-    // More info can be found here: https://docs.opensea.io/docs/metadata-standards
+      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
     res.status(200).json({
-      name: "Solidity logo #" + tokenId,
-      description: "Solidity is an object-oriented programming language for implementing smart contracts on various blockchain platforms, most notably, Ethereum. ",
+      name: "SUPPORT ETHEREUM #" + tokenId,
+      description: "Ethereum is a decentralized, open-source blockchain with smart contract functionality. Ether (ETH or Ξ) is the native cryptocurrency of the platform. Among cryptocurrencies, Ether is second only to Bitcoin in market capitalization",
       image: image_url + tokenId + ".svg",
     });
   }
-  
